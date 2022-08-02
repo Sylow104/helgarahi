69f8f873baacac6b1e65afc5b386eef713e94dbc
Patrick Ching <burning.codex@gmail.com>
2022-08-02 09:22:18 -0600

    making a cycled in workbook generator

--- src/workbook.c
+++ src/workbook.c
@@ -1,8 +1,15 @@
 #include "helgarahi.h"
+#include <string.h>
 #define WORKBOOK_INFO "xl/workbook.xml"
+#define WORKBOOK_SHEET " xl/worksheets/sheet%s.xml"
 
 void XMLCALL workbook_start(void *data, const char *tag, const char **attr)
 {
+	size_t *_data = (size_t *) data;
+
+	if (!strcmp(tag, "sheet")) {
+		(*_data)++;
+	}
 	return;
 }
 
@@ -11,19 +18,22 @@ void XMLCALL workbook_end(void *data, const char *tag)
 	return;
 }
 
-
 size_t workbook_num_sheets(xml_t *raw)
 {
-	XML_Parser parser = XML_ParserCreate(raw->buffer);
-	size_t num_sheets;
+	XML_Parser parser = XML_ParserCreate("US-ASCII");
+	size_t num_sheets = 0;
 	enum XML_Status status;
 	if (!parser) {
 		return 0;
 	}
 
 	XML_SetElementHandler(parser, workbook_start, workbook_end);
-	status = XML_Parse(parser, raw->buffer, raw->size, 0);
-
+	XML_SetUserData(parser, &num_sheets);
+	status = XML_Parse(parser, raw->buffer, raw->read_size, 0);
+	if (status == XML_STATUS_ERROR) {
+		printf("Error Parsing: %s\n", 
+			XML_ErrorString(XML_GetErrorCode(parser)));
+	}
 	XML_ParserFree(parser);
 	return num_sheets;
 }
@@ -32,11 +42,13 @@ workbook_t *workbook_generate(xlsx_t *target)
 {
 	workbook_t *to_ret = (workbook_t *) malloc(sizeof(workbook_t));
 	xml_t *workbook_raw;
+	xml_t *cur_sheet = 0x0;
+	char buffer[128];
 	if (!to_ret) {
 		goto exit;
 	}
 
-	workbook_raw = xml_load(WORKBOOK_INFO, target->zip);
+	workbook_raw = xml_load(WORKBOOK_INFO, target->zip, 0x0);
 	if (!workbook_raw) {
 		goto on_fail;
 	}
@@ -46,7 +58,20 @@ workbook_t *workbook_generate(xlsx_t *target)
 		xml_free(workbook_raw);
 		goto on_fail;
 	}
+
+	// generate sheets
+	to_ret->sheets = (sheet_t *) 
+		malloc(sizeof(sheet_t) * to_ret->num_sheets);
+	if (!to_ret->sheets) {
+		xml_free(workbook_raw);
+		goto on_fail;
+	}
+	for (size_t i = 0; i < to_ret->num_sheets; i++) {
+		snprintf(buffer, 128, WORKBOOK_SHEET, i);
+		cur_sheet = xml_load(buffer, target->zip, cur_sheet);
+	}
 	
+	xml_free(cur_sheet);
 	xml_free(workbook_raw);
 exit:
 	return to_ret;
@@ -54,4 +79,15 @@ on_fail:
 	free(to_ret);
 	to_ret = 0x0;
 	goto exit;
+}
+
+int workbook_clean(workbook_t *obj)
+{
+	if (obj->num_sheets) {
+		for (size_t i = 0; i < obj->num_sheets; i++) {
+			//sheet_clean(obj->sheets);
+		}
+		//free(obj->sheets);
+	}
+	return 0;
 }
