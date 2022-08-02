9667ab98861abc904601d97b8e2330d28970ed12
Patrick Ching <burning.codex@gmail.com>
2022-08-02 13:35:09 -0600

    change workbook to stack instead of heap

--- src/workbook.c
+++ src/workbook.c
@@ -38,50 +38,49 @@ size_t workbook_num_sheets(xml_t *raw)
 	return num_sheets;
 }
 
-workbook_t *workbook_generate(xlsx_t *target)
+int workbook_generate(xlsx_t *target)
 {
-	workbook_t *to_ret = (workbook_t *) malloc(sizeof(workbook_t));
+	int to_ret = 0;
+	workbook_t *to_use = &target->workbook;
 	xml_t *workbook_raw;
 	xml_t *cur_xml = 0x0;
 	char buffer[128];
-	if (!to_ret) {
-		goto exit;
-	}
 
-	workbook_raw = xml_load(WORKBOOK_INFO, target->zip, 0x0);
+	workbook_raw = xml_load(WORKBOOK_INFO, target->zip);
 	if (!workbook_raw) {
 		goto on_fail;
 	}
 	// get the number of sheets in the workbook
-	to_ret->num_sheets = workbook_num_sheets(workbook_raw);
+	to_use->num_sheets = workbook_num_sheets(workbook_raw);
 	
-	if (!to_ret->num_sheets) {
+	if (!to_use->num_sheets) {
 		goto on_fail;
 	}
+	xml_free(workbook_raw);
 
 	// generate sheets
-	to_ret->sheets = (sheet_t *) 
-		malloc(sizeof(sheet_t) * to_ret->num_sheets);
-	if (!to_ret->sheets) {
+	to_use->sheets = (sheet_t *) 
+		malloc(sizeof(sheet_t) * to_use->num_sheets);
+	if (!to_use->sheets) {
 		goto on_fail;
 	}
 
-	for (size_t i = 0; i < to_ret->num_sheets; i++) {
+	for (size_t i = 0; i < to_use->num_sheets; i++) {
 		snprintf(buffer, 128, WORKBOOK_SHEET, i + 1);
-		cur_xml = xml_load(buffer, target->zip, cur_xml);
-		if (sheet_generate(&to_ret->sheets[i], cur_xml)) {
-			xml_free(cur_xml);
+		cur_xml = xml_load(buffer, target->zip);
+		if (sheet_generate(&to_use->sheets[i], cur_xml)) {
 			goto on_fail;
 		}
+		xml_free(cur_xml);
 	}
-
-	xml_free(cur_xml);
 	
 exit:
 	return to_ret;
 on_fail:
-	free(to_ret);
-	to_ret = 0x0;
+	to_ret = -1;
+	if (cur_xml) {
+		xml_free(cur_xml);
+	}
 	goto exit;
 }
 
@@ -89,9 +88,9 @@ int workbook_clean(workbook_t *obj)
 {
 	if (obj->num_sheets) {
 		for (size_t i = 0; i < obj->num_sheets; i++) {
-			//sheet_clean(obj->sheets);
+			sheet_clean(&obj->sheets[i]);
 		}
-		//free(obj->sheets);
+		free(obj->sheets);
 	}
 	return 0;
 }
