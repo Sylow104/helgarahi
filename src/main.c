415f6bda8d0680bf94378cc18e3ecc619585ba92
Patrick Ching <burning.codex@gmail.com>
2022-08-02 08:05:16 -0600

    using main for testing

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "helgarahi.h"
+
+int main(int argc, const char **argv)
+{
+	xlsx_t *to_use = xlsx_open(argv[1]);
+	if (!to_use) {
+		exit(-1);
+	}
+
+	return xlsx_close(to_use);
+}
