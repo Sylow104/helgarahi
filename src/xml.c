5c0709d576b7a387cf9955dd23cfd4ff83b9a8e4
Patrick Ching <burning.codex@gmail.com>
2022-08-02 09:23:09 -0600

    rewrote the xml_load with a recursive functions on future calls to reuse a xml_t buffer

--- src/xml.c
+++ src/xml.c
@@ -1,25 +1,36 @@
 #include "helgarahi.h"
 
-xml_t *xml_load(const char *file, zip_t *zip)
+xml_t *xml_load(const char *file, zip_t *zip, xml_t *to_reload)
 {
 	xml_t *to_ret;
 	zip_file_t *to_dump;
 	zip_stat_t stat;
 	int rc;
 
-	to_ret = (xml_t *) malloc(sizeof(xml_t));
+	if (!(file && zip)) {
+		return 0x0;
+	}
+
+	if (to_reload) {
+		to_ret = to_reload;
+	} else {
+		to_ret = (xml_t *) malloc(sizeof(xml_t));
+		to_ret->read_size = 0;
+		to_ret->buffer_size = 0;
+	}
+
 	if (!to_ret) {
 		goto exit;
 	}
 
-	rc = zip_stat(zip, file, 0, &stat);
-	if (rc) {
+	if (zip_stat(zip, file, 0, &stat)) {
 		goto exit;
 	}
 
-	to_ret->buffer = (char *) malloc(sizeof(char) * (1 + stat.size));
-	if (!to_ret->buffer) {
-		goto on_fail;
+	if (to_ret->buffer_size < stat.size) {
+		to_ret->buffer = (char *) 
+			realloc(to_ret->buffer, sizeof(char) * (stat.size));
+		to_ret->buffer_size = stat.size;
 	}
 
 	to_dump = zip_fopen(zip, file, 0);
@@ -27,15 +38,15 @@ xml_t *xml_load(const char *file, zip_t *zip)
 		goto on_fail;
 	}
 
-	to_ret->size = zip_fread(to_dump, to_ret->buffer, stat.size);
+	to_ret->read_size = zip_fread(to_dump, to_ret->buffer, stat.size);
 	zip_fclose(to_dump);
-	if (to_ret->size != stat.size) {
+	if (to_ret->read_size != stat.size) {
 		goto on_fail;
 	}
 
-
 exit:
 	return to_ret;
+
 on_fail:
 	if (to_ret->buffer) {
 		free(to_ret->buffer);
