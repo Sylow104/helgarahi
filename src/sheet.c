c15f475e09680b6dcadaf865607c990bb976f148
Patrick Ching <burning.codex@gmail.com>
2022-08-02 09:21:55 -0600

    modified to use xml_t sheet analysis

--- src/sheet.c
+++ src/sheet.c
@@ -1,11 +1,12 @@
 #include "helgarahi.h"
 
-sheet_t *sheet_generate(zip_t *xlsx_file)
+sheet_t *sheet_generate(xml_t *raw)
 {
 	return 0x0;
 }
 
-int sheet_destroy(sheet_t *obj)
+int sheet_clean(sheet_t *obj)
 {
+	free(obj);
 	return 0;
 }
