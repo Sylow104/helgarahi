ace07cc035added148f7755eac38e679497aa57f
Patrick Ching <burning.codex@gmail.com>
2022-08-02 09:23:15 -0600

    misc spacing

--- src/main.c
+++ src/main.c
@@ -4,9 +4,12 @@
 int main(int argc, const char **argv)
 {
 	xlsx_t *to_use = xlsx_open(argv[1]);
+	
 	if (!to_use) {
 		exit(-1);
 	}
+
+	workbook_t *workbook = workbook_generate(to_use);
 
 	return xlsx_close(to_use);
 }
